64ab0a2b375a03d1506b6191fe897358c83c7aa3
nrkhlifah28 <94022360+nrkhlifah28@users.noreply.github.com>
2022-09-13 07:06:51 +0700

    Update main-request.js

--- plugins/main-request.js
+++ plugins/main-request.js
@@ -7,7 +7,7 @@ let handler = async(m, { conn, text, usedPrefix, command}) => {
     if (text > 300) return conn.reply(m.chat, 'Maaf Teks Terlalu Panjang, Maksimal 300 Teks', m)
     var nomor = m.sender
     const teks1 = `*[ 𝗥𝗲𝗾𝘂𝗲𝘀𝘁 ]*\nNomor : wa.me/${nomor.split("@s.whatsapp.net")[0]}\nPesan : ${text}`
-    conn.reply('6281361281833@s.whatsapp.net', teks1, m)
+    conn.reply('6289618728196@s.whatsapp.net', teks1, m)
     conn.reply(m.chat, '✔️ Masalah berhasil dikirimkan ke Owner', m)
 }
 handler.help = ['request <fitur>']
